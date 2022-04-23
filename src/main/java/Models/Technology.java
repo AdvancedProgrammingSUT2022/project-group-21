4fcf920f61b44b08e0f87590a30e5bff5734b465
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-04-23 17:22:41 +0430

    add Technology

--- src/main/java/Models/Technology.java
+++ src/main/java/Models/Technology.java
@@ -1,4 +1,19 @@
 package Models;
 
-public class Technology {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public enum Technology{
+	Agriculture(20, null, new ArrayList<Technology>(Arrays.asList())),
+	;
+
+	public final int cost;
+	public final Era era;
+	public final ArrayList<Technology> prequisiteTechs;
+
+	Technology(int cost, Era era, ArrayList<Technology> prequisiteTechs){
+		this.cost=cost;
+		this.era=era;
+		this.prequisiteTechs=prequisiteTechs;
+	}
 }
