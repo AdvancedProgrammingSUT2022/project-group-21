862a9700292fcabf92d5c5c70b925aa11e619e6f
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-04-24 23:58:35 +0430

    small change

--- src/main/java/Models/Civilization.java
+++ src/main/java/Models/Civilization.java
@@ -8,28 +8,33 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Civilization {
-    private ArrayList<City> cities;
-    private City capitalCity;
-    private HashSet<Technology> technologies;
-    private HashMap<Resource, Integer> resources;
-    private int happiness;
-    private boolean[][] fogOfWar;
-    private ArrayList<Unit> units;
+	private ArrayList<City> cities;
+	private City capitalCity;
+	private HashSet<Technology> technologies;
+	private HashMap<Resource, Integer> resources;
+	private int happiness;
+	private boolean[][] fogOfWar;
+	private ArrayList<Unit> units;
 
 
-    public Civilization(Tile tile) {
-        this.capitalCity = new City(0, 0 , "", tile);
-    }
+	public Civilization(Tile tile) {
+		this.capitalCity = new City(0, 0 , "", tile);
+	}
 
-    public int hasTechnology(Technology technology) {
-        //TODO
-        return 0;
-    }
-    public int countResource(Resource resource) {
-        return resources.get(resource);
-    }
-    public boolean isTileVisible(Tile tile){
-        //TODO
-        return false;
-    }
+	public int hasTechnology(Technology technology) {
+		//TODO
+		return 0;
+	}
+	public int countResource(Resource resource) {
+		return resources.get(resource);
+	}
+	public boolean isTileVisible(Tile tile){
+		//TODO
+		return false;
+	}
+
+	public void setVisible(int x, int y){
+		// TODO
+	}
+
 }
--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -9,9 +9,16 @@ public abstract class Menu {
     protected static Scanner getScanner() {
         return scanner;
     }
-    protected String getInput() {
-        return scanner.nextLine();
-    }
+    protected String getInput(){
+		String input=scanner.nextLine();
+		String S[]=input.split("\\s");
+		String T="";
+		for (String string : S) if (string.length()>0){
+			if (T.length()>0) T+=" ";
+			T+=string;
+		}
+		return T;
+	}
     public abstract Menu run();
     protected abstract void showCurrentMenu();
     protected abstract void enterMenu();
