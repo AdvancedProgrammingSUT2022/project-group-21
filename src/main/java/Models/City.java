4d5a03c4a0bceae4ece1d55c65fd54af353ed208
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-05-01 18:04:31 +0430

    fix City

--- src/main/java/Models/City.java
+++ src/main/java/Models/City.java
@@ -4,69 +4,61 @@ import Models.Unit.MilitaryUnit;
 import java.util.ArrayList;
 
 public class City {
-    private final ArrayList<Tile> tiles = new ArrayList<>();
-    private Civilization civilization;
-    private int gold;
-    private int food;
-    private boolean isCapital;
-    private ArrayList<Citizen> citizens;
-    private Tile tile;
-    private MilitaryUnit garrisonUnit;
-    private int combatStrength;
+	private final ArrayList<Tile> tiles = new ArrayList<>();
+	private Civilization civilization;
+	private int gold;
+	private int food;
+	private boolean isCapital;
+	private ArrayList<Citizen> citizens;
+	private Tile tile;
+	private MilitaryUnit garrisonUnit;
+	private int combatStrength;
 
-    public City(int gold, int food, Tile tile) {
-        this.gold = gold;
-        this.food = food;
-        this.tile = tile;
-    }
-    public boolean isTileVisible(int x, int y) {
-        //TODO
-        return false;
-    }
+	public City(int gold, int food, Tile tile) {
+		this.gold = gold;
+		this.food = food;
+		this.tile = tile;
+	}
+	
+	public double getCombatStrength(int thisTurn) {
+		// TODO: exponential
+		double number = this.combatStrength;
+		if (this.tile.getMilitaryUnit().isOnGarrison()){
+			for (int i = this.tile.getMilitaryUnit().getLastActionTurn();i<thisTurn;i++){
+				number = 1.25 * number;
+			}
+		}
+		return number;
+	}
 
-    public double getCombatStrength(int thisTurn) {
-        double number = this.combatStrength;
-        if (this.tile.getMilitaryUnit().isOnGarrison()){
-            for (int i = this.tile.getMilitaryUnit().getLastActionTurn();i<thisTurn;i++){
-                number = 1.25 * number;
-            }
-        }
-        return number;
-    }
+	public void lockCitizen(Tile tile) {
+		Citizen citizen = getCitizenToAssign();
+		citizen.assignToTile(tile);
+	}
+	private Citizen getCitizenToAssign(){
+		for (Citizen citizen : citizens){
+			if (!citizen.isWorking())
+				return citizen;
+		}
+		int index = (int)(Math.random() * citizens.size());
+		return citizens.get(index);
+	}
 
-    public void lockCitizen(Tile tile) {
-        Citizen citizen = getCitizenToAssign();
-        citizen.assignToTile(tile);
-    }
-    private  Citizen getCitizenToAssign() {
-        for (Citizen citizen : citizens) {
-            if (!citizen.isWorking())
-                return citizen;
-        }
-        int index = (int)(Math.random() * citizens.size());
-        return citizens.get(index);
-    }
+	public void removeCitizen(Citizen citizen) {
+		citizens.remove(citizen);
+	}
 
-    public void removeCitizen(Citizen citizen) {
+	public void removeCitizenFromWork(Citizen citizen){
+		citizen.getWorkingTile().setCitizen(null);
+		citizen.setWorkingTile(null);
+	}
 
-        citizens.remove(citizen);
-    }
+	public void addTile(Tile tile) {
+		tiles.add(tile);
+	}
 
-    public void removeCitizenFromWork(Tile tile) {
-        tile.setCitizen(null);
-        tile.getWorkingCitizen().setWorkingTile(null);
-    }
-
-    public ArrayList<Tile> getTiles() {
-        return tiles;
-    }
-
-    public void purchaseTile(Tile tile) {
-        tiles.add(tile);
-    }
-
-    public void addCitizen() {
-        citizens.add(new Citizen(this));
-    }
+	public void addCitizen() {
+		citizens.add(new Citizen(this));
+	}
 
 }
