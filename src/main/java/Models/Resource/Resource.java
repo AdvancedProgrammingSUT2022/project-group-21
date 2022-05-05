bfc35b40e260f1dbfbfc566919272c9a90b6b8a7
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-05-05 20:15:58 +0430

    edit Resource

--- src/main/java/Models/Resource/Resource.java
+++ src/main/java/Models/Resource/Resource.java
@@ -3,24 +3,30 @@ package Models.Resource;
 import Models.Civilization;
 import Models.Improvement;
 import Models.Technology;
-import Models.Terrain;
 
 public enum Resource {
-	; // TODO
+	MOZ1(ResourceType.BONUS, -1, -1, -1, Improvement.MANUFACTORY, Technology.CHEMISTRY),
+
+	MOZ2(ResourceType.STRATEGIC, -1, -1, -1, Improvement.MANUFACTORY, Technology.CHEMISTRY),
+	
+	MOZ3(ResourceType.LUXURY, -1, -1, -1, Improvement.MANUFACTORY, Technology.CHEMISTRY),
+	; 
+	
+	
 	final public ResourceType resourceType;
 	final public int food;
 	final public int production;
 	final public int gold;
-	final public Terrain canBeFoundOn[];
+	// final public Terrain canBeFoundOn[];
 	final public Improvement improvementNeeded;
 	final public Technology technologyNeeded;
 
-	private Resource(ResourceType resourceType, int food, int production, int gold, Terrain canBeFoundOn[], Improvement improvementNeeded, Technology technologyNeeded){
+	private Resource(ResourceType resourceType, int food, int production, int gold, Improvement improvementNeeded, Technology technologyNeeded){
 		this.resourceType=resourceType;
 		this.food=food;
 		this.production=production;
 		this.gold=gold;
-		this.canBeFoundOn=canBeFoundOn;
+		// this.canBeFoundOn=canBeFoundOn;
 		this.improvementNeeded=improvementNeeded;
 		this.technologyNeeded=technologyNeeded;
 	}
