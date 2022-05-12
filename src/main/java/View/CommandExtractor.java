a3b5c977459273c7dbc817946863c65bb913332d
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-05-12 17:51:51 +0430

    commandExtractor

--- src/main/java/View/CommandExtractor.java
+++ src/main/java/View/CommandExtractor.java
@@ -3,15 +3,26 @@ package View;
 import Enums.InputCommand;
 
 import java.util.HashMap;
+import java.util.regex.Matcher;
 
 public class CommandExtractor {
-	InputCommand command;
-	HashMap<String, String> args;
+	public InputCommand command;
+	public HashMap<String, String> args;
 
+	private CommandExtractor(InputCommand inputCommand, Matcher matcher){
+		this.command=inputCommand;
+		this.args=new HashMap<>();
+		for (String[] arg : inputCommand.args) {
+			args.put(arg[0], matcher.group(arg[0]));
+		}
+	}
 
 	public static CommandExtractor extractCommand(String input){
-		
-		
+		for (InputCommand inputCommand : InputCommand.values()){
+			if (!input.startsWith(inputCommand.command)) continue ;
+			Matcher matcher=inputCommand.pattern.matcher(input);
+			if (matcher.matches()) return new CommandExtractor(inputCommand, matcher);
+		}
 		return null;
 	}
 }
