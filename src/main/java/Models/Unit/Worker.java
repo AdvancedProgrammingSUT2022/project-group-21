78c6336551e66b99622babb8fa5a3ae88a37002b
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-05-15 17:09:13 +0430

    worker

--- src/main/java/Models/Unit/Worker.java
+++ src/main/java/Models/Unit/Worker.java
@@ -1,33 +1,47 @@
 package Models.Unit;
 
 import Models.Civilization;
+import Models.Tile.Improvement;
 import Models.Tile.Tile;
 
 public class Worker extends CivilianUnit{
 	
+	private ProjectType projectType;
 	private int projectTurnsLeft;
-	
+	private Improvement projectImprovement; // if profectType==BUILD_IMPROVEMENT
 
 	public Worker(Civilization owner, Tile tile){
 		super(UnitType.WORKER, owner, tile);
 	}
 
 	public int getProjectTurnsLeft(){ return projectTurnsLeft;}
+	public void decreaseProjectTurnsLeft(){
+		projectTurnsLeft--;
+		if (projectTurnsLeft>0) return ;
+		// TODO
+	}
 
 
+	public void buildRoad(){
+		projectType=ProjectType.BUILD_ROAD;
+		projectTurnsLeft=3;
+	}
+	public void buildImprovement(Improvement improvement){
+		// TODO
+	}
+	public void removeJungle(){
+		// TODO
+	}
+	public void cancelProject(){
+		// TODO
+	}
+	
+	
+	
+}
 
-	public void creatRoad(Tile tile) {}
-	public void creatFarm(Tile tile) {}
-	public void leaveOlderAndCreatNewer(Tile tile) {}
-	public void repairRoad(Tile tile) {}
-	public void createMine(Tile tile) {}
-	public void createTradingPost(Tile tile) {}
-	public void createWoodFactory(Tile tile) {}
-	public void createCamp(Tile tile) {}
-	public void createPasture(Tile tile) {}
-	public void removeRoad(Tile tile) {}
-	public void removeForest(Tile tile) {}
-	public void removeJungle(Tile tile) {}
-	public void removeMarsh(Tile tile) {}
-	public void createQuarry(Tile tile) {}
+enum ProjectType{
+	BUILD_ROAD,
+	BUILD_IMPROVEMENT,
+	REMOVE_JUNGLE;
 }
