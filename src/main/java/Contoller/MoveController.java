770fa4d86620aff9ab56d53ef0f0ef772cf90c3b
Ali <alialvandi99@yahoo.com>
2022-05-16 21:45:33 +0430

    Move Controller final

--- src/main/java/Contoller/MoveController.java
+++ src/main/java/Contoller/MoveController.java
@@ -19,22 +19,98 @@ public class MoveController {
 		if (instance == null) MoveController.setInstance(new MoveController());
 		return instance;
 	}
-	/*public String  moveUnit(int x, int y) {
-	 	Unit selectedUnit = SelectController.getInstance().getSelectedUnit();
-	 	Tile selectedTile = GameController.getInstance().getGame().getTile(x, y);
-	 	if (selectedUnit == null)
-	 		return "You have not selected any unit";
-	 	if (selectedUnit.owner != GameController.getInstance().getGame().getCurrentPlayer().getCivilization())
-	 		return "this unit is not yours";
-	 	int mp = selectedUnit.getMP();
-	 	//TODO
-	 	if (Math.abs(selectedUnit.getTile().X - x) + Math.abs(selectedUnit.getTile().Y - y) > mp)
-	 		return Message.OUT_OF_MP.toString();
-	 	selectedUnit.setTile(selectedTile);
-		return Message.SUCCESS.toString();
-	}*/
 
 	public Message moveUnit(int x, int y){
+		Unit selectedUnit = SelectController.getInstance().getSelectedUnit();
+		Tile selectedTile = GameController.getInstance().getGame().getTile(x,y);
+		if (selectedUnit == null)
+			return Message.FAIL;
+		if (selectedUnit.owner != GameController.getInstance().getGame().getCurrentPlayer().getCivilization())
+			return Message.FAIL;
+		if (!GameController.getInstance().getGame().getCurrentPlayer().getCivilization().isTileVisible(x,y)){
+			return Message.FAIL;
+		}
+		ArrayList<Tile> path = getPath(selectedUnit.getTile(),selectedTile);
+		int i;
+		int j;
+		int MP = selectedUnit.getMP();
+		if (path == null) return Message.FAIL;
+		if (selectedUnit instanceof MilitaryUnit){
+			while (selectedUnit.getMP() != 0 && !path.get(0).equals(selectedTile)){
+				for (i = 1;i<path.size();i++) {
+					if (path.get(i).getMilitaryUnit() == null) {
+						break;
+						//We have no path to go
+					}
+				}
+				if (i == path.size()){
+					return Message.FAIL;
+				}
+				for (j = 0;j<=i-1;j++){
+					if (MP == 0){
+						break;
+					}
+					if (riverPass(path.get(j),path.get(j+1))){
+						MP = 0;
+					}
+					else {
+						MP = MP - path.get(j+1).getMovementCost();
+						if (MP < 0){
+							MP = 0;
+						}
+					}
+				}
+				if (j == i){
+					selectedUnit.setTile(path.get(i));
+					selectedUnit.setMP(MP);
+					for (int k = 0;k<i;k++){
+						path.remove(0);
+					}
+				}
+				else {
+					return Message.FAIL;
+					//Aborting
+				}
+			}
+
+		}
+		if (selectedUnit instanceof CivilianUnit){
+			while (selectedUnit.getMP() != 0 && !path.get(0).equals(selectedTile)){
+				for (i = 1;i<path.size();i++) {
+					if (path.get(i).getCivilianUnit() == null) {
+						break;
+					}
+				}
+				if (i == path.size()){
+					return Message.FAIL;
+				}
+				for (j = 0;j<=i-1;j++){
+					if (MP == 0){
+						break;
+					}
+					if (riverPass(path.get(j),path.get(j+1))){
+						MP = 0;
+					}
+					else {
+						MP = MP - path.get(j+1).getMovementCost();
+						if (MP < 0){
+							MP = 0;
+						}
+					}
+				}
+				if (j == i){
+					selectedUnit.setTile(path.get(i));
+					selectedUnit.setMP(MP);
+					for (int k = 0;k<i;k++){
+						path.remove(0);
+					}
+				}
+				else {
+					return Message.FAIL;
+					//Aborting
+				}
+			}
+		}
 		// TODO
 		// move selected unit to tile (x, y)
 		// if tile was not visible: FAIL
@@ -46,86 +122,7 @@ public class MoveController {
 
 
 	
-	public boolean shitMove(Unit unit,Tile firstTile,Tile lastTile){
-		ArrayList<Tile> path = getPath(firstTile,lastTile);
-		int i;
-		int j;
-		int MP = unit.getMP();
-		if (path == null) return true;
-		if (unit instanceof MilitaryUnit){
-			while (unit.getMP() != 0 && !path.get(0).equals(lastTile)){
-				for (i = 1;i<path.size();i++) {
-					if (path.get(i).getMilitaryUnit() == null) {
-						break;
-						//We have no path to go
-					}
-				}
-				for (j = 0;j<=i-1;j++){
-					if (MP == 0){
-						break;
-					}
-					if (riverPass(path.get(j),path.get(j+1))){
-						MP = 0;
-					}
-					else {
-						MP = MP - path.get(j+1).getMovementCost();
-						if (MP < 0){
-							MP = 0;
-						}
-					}
-				}
-				if (j == i){
-					unit.setTile(path.get(i));
-					unit.setMP(MP);
-					for (int k = 0;k<i;k++){
-						path.remove(0);
-					}
-				}
-				else {
-					return true;
-					//Aborting
-				}
-			}
 
-		}
-		if (unit instanceof CivilianUnit){
-			while (unit.getMP() != 0 && !path.get(0).equals(lastTile)){
-				for (i = 1;i<path.size();i++) {
-					if (path.get(i).getCivilianUnit() == null) {
-						break;
-					}
-				}
-				for (j = 0;j<=i-1;j++){
-					if (MP == 0){
-						break;
-					}
-					if (riverPass(path.get(j),path.get(j+1))){
-						MP = 0;
-					}
-					else {
-						MP = MP - path.get(j+1).getMovementCost();
-						if (MP < 0){
-							MP = 0;
-						}
-					}
-				}
-				if (j == i){
-					unit.setTile(path.get(i));
-					unit.setMP(MP);
-					for (int k = 0;k<i;k++){
-						path.remove(0);
-					}
-				}
-				else {
-					return true;
-					//Aborting
-				}
-			}
-		}
-		return path.get(0).equals(lastTile);
-		//We return true if we end the path or cant go to tile in the path
-		//returning false for multi-turn movement that needs TODO...
-	}
 
 	public ArrayList<Tile> getPath(Tile firstTile, Tile lastTile){
 		// run spfa for shortest path
