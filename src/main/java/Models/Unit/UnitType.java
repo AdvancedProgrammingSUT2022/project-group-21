4a696e6f0a1cc8407ae94fa549d7499358e9f249
alishahali1382 <67241070+alishahali1382@users.noreply.github.com>
2022-06-16 01:05:43 +0430

    add Unit Notes

--- src/main/java/Models/Unit/UnitType.java
+++ src/main/java/Models/Unit/UnitType.java
@@ -5,81 +5,179 @@ import Models.Technology;
 import Models.Resource.Resource;
 import Models.Tile.Tile;
 
-public enum UnitType{
-	SETTLER(CombatType.CIVILIAN, 89, -1, -1, -1, 2, null, false, new Technology[]{}),
-	WORKER(CombatType.CIVILIAN, 70, -1, -1, -1, 2, null, false, new Technology[]{}),
+public enum UnitType {
+	SETTLER(CombatType.CIVILIAN, 89, -1, -1, -1, 2, null,
+			new Technology[] {},
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.FOUND_CITY },
+			new UnitAbility[] {}),
 
+	WORKER(CombatType.CIVILIAN, 70, -1, -1, -1, 2, null,
+			new Technology[] {},
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.WORKER_ACTION },
+			new UnitAbility[] {}),
 
-	ARCHER(CombatType.ARCHERY, 70, 4, 6, 2, 2, null, false,
-		new Technology[]{Technology.ARCHERY}
-	),
-	CHARIOTARCHER(CombatType.MOUNTED,60,3,6,2,4,Resource.HORSE,false,
-		new Technology[]{Technology.THE_WHEEL}
-	),
-	SCOUT(CombatType.RECON,25,4,-1,-1,2,null, false,
-		null
-	),
-	SPEARMAN(CombatType.MELEE,50,7,-1,-1,2,null, true,
-			new Technology[]{Technology.BRONZE_WORKING}
-	),
-	WARRIOR(CombatType.MELEE,40,6,-1,-1,2,null, true,
-			null
-	),
-	CATAPULT(CombatType.SIEGE,100,4,14,2,2,Resource.IRON, false,
-			new Technology[]{Technology.MATHEMATICS}
-	),
-	HORSEMAN(CombatType.MOUNTED,80,12,-1,-1,4,Resource.HORSE, true,
-			new Technology[]{Technology.HORSEBACK_RIDING}
-	),
-	SWORDSMAN(CombatType.MELEE,80,11,-1,-1,2,Resource.IRON, true,
-			new Technology[]{Technology.IRON_WORKING}
-	),
-	CROSSBOWMAN(CombatType.ARCHERY,120,6,12,2,2,null,false,
-			new Technology[]{Technology.MACHINERY}
-	),
-	KNIGHT(CombatType.MOUNTED,150,18,-1,-1,3,Resource.HORSE, true,
-			new Technology[]{Technology.CHIVALRY}
-	),
-	Longswordsman(CombatType.MELEE,150,18,-1,-1,3,Resource.IRON, true,
-			new Technology[]{Technology.STEEL}
-	),
-	Pikeman(CombatType.MELEE,100,10,-1,-1,2,null, true,
-			new Technology[]{Technology.CIVIL_SERVICE}
-	),
-	Trebuchet(CombatType.SIEGE,170,6,20,2,2,Resource.IRON, false,
-			new Technology[]{Technology.PHYSICS}
-	),
-	Canon(CombatType.SIEGE,250,10,26,2,2,null, false,
-			new Technology[]{Technology.CHEMISTRY}
-	),
-	Cavalry(CombatType.MOUNTED,260,25,-1,-1,3,Resource.HORSE, true,
-			new Technology[]{Technology.MILITARY_SCIENCE}
-	),
-	Lancer(CombatType.MOUNTED,220,22,-1,-1,4,Resource.HORSE, true,
-			new Technology[]{Technology.METALLURGY}
-	),
-	Musketman(CombatType.GUNPOWDER,120,16,-1,-1,2,null, true,
-			new Technology[]{Technology.GUNPOWDER}
-	),
-	Rifleman(CombatType.GUNPOWDER,200,25,-1,-1,2,null, true,
-			new Technology[]{Technology.RIFLING}
-	),
-	AntiTankGun(CombatType.GUNPOWDER,300,32,-1,-1,2,null, true,
-			new Technology[]{Technology.REPLACEABLE_PARTS}
-	),
-	Artillery(CombatType.SIEGE,420,16,32,3,2,null, false,
-			new Technology[]{Technology.DYNAMITE}
-	),
-	Infantry(CombatType.GUNPOWDER,300,36,-1,-1,2,null, true,
-			new Technology[]{Technology.REPLACEABLE_PARTS}
-	),
-	Panzer(CombatType.ARMORED,450,60,-1,-1,5,null, true,
-			new Technology[]{Technology.COMBUSTION}
-	),
-	Tank(CombatType.ARMORED,450,50,-1,-1,4,null, true,
-			new Technology[]{Technology.COMBUSTION}
-	),
-	;
+	ARCHER(CombatType.ARCHERY, 70, 4, 6, 2, 2, null,
+			new Technology[] { Technology.ARCHERY },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK },
+			new UnitAbility[] {}),
+
+	CHARIOTARCHER(CombatType.MOUNTED, 60, 3, 6, 2, 4, Resource.HORSE,
+			new Technology[] { Technology.THE_WHEEL },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK },
+			new UnitAbility[] {UnitAbility.ROUGH_TERRAIN_PENALTY, UnitAbility.NO_DEFENSIVE_BONUS}),
+
+	SCOUT(CombatType.RECON, 25, 4, -1, -1, 2, null,
+			new Technology[] {},
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.IGNORE_TERRAIN_COST}),
+
+	SPEARMAN(CombatType.MELEE, 50, 7, -1, -1, 2, null,
+			new Technology[] { Technology.BRONZE_WORKING },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.BONUS_VS_MOUNTED_100}),
+
+	WARRIOR(CombatType.MELEE, 40, 6, -1, -1, 2, null,
+			new Technology[] {},
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {}),
+
+	CATAPULT(CombatType.SIEGE, 100, 4, 14, 2, 2, Resource.IRON,
+			new Technology[] { Technology.MATHEMATICS },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK, UnitActionType.PRE_ATTACK_SETUP },
+			new UnitAbility[] {UnitAbility.BONUS_VS_CITY_10, UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.LIMITED_VISIBILITY}),
+
+	HORSEMAN(CombatType.MOUNTED, 80, 12, -1, -1, 4, Resource.HORSE,
+			new Technology[] { Technology.HORSEBACK_RIDING },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.CAN_MOVE_AFTER_ATTACK}),
+
+	SWORDSMAN(CombatType.MELEE, 80, 11, -1, -1, 2, Resource.IRON,
+			new Technology[] { Technology.IRON_WORKING },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {}),
+
+	CROSSBOWMAN(CombatType.ARCHERY, 120, 6, 12, 2, 2, null,
+			new Technology[] { Technology.MACHINERY },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK },
+			new UnitAbility[] {}),
+
+	KNIGHT(CombatType.MOUNTED, 150, 18, -1, -1, 3, Resource.HORSE,
+			new Technology[] { Technology.CHIVALRY },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.CAN_MOVE_AFTER_ATTACK}),
+
+	LONGSWORDSMAN(CombatType.MELEE, 150, 18, -1, -1, 3, Resource.IRON,
+			new Technology[] { Technology.STEEL },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {}),
+
+	PIKEMAN(CombatType.MELEE, 100, 10, -1, -1, 2, null,
+			new Technology[] { Technology.CIVIL_SERVICE },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.BONUS_VS_MOUNTED_100}),
+
+	TREBUCHET(CombatType.SIEGE, 170, 6, 20, 2, 2, Resource.IRON,
+			new Technology[] { Technology.PHYSICS },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK, UnitActionType.PRE_ATTACK_SETUP },
+			new UnitAbility[] {UnitAbility.BONUS_VS_CITY_10, UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.LIMITED_VISIBILITY, }),
+
+	CANON(CombatType.SIEGE, 250, 10, 26, 2, 2, null,
+			new Technology[] { Technology.CHEMISTRY },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK, UnitActionType.PRE_ATTACK_SETUP },
+			new UnitAbility[] {UnitAbility.BONUS_VS_CITY_10, UnitAbility.NO_DEFENSIVE_BONUS}),
+
+	CAVALRY(CombatType.MOUNTED, 260, 25, -1, -1, 3, Resource.HORSE,
+			new Technology[] { Technology.MILITARY_SCIENCE },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.CAN_MOVE_AFTER_ATTACK}),
+
+	LANCER(CombatType.MOUNTED, 220, 22, -1, -1, 4, Resource.HORSE,
+			new Technology[] { Technology.METALLURGY },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.CAN_MOVE_AFTER_ATTACK}),
+
+	MUSKETMAN(CombatType.GUNPOWDER, 120, 16, -1, -1, 2, null,
+			new Technology[] { Technology.GUNPOWDER },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {}),
+
+	RIFLEMAN(CombatType.GUNPOWDER, 200, 25, -1, -1, 2, null,
+			new Technology[] { Technology.RIFLING },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {}),
+
+	ANTITANKGUN(CombatType.GUNPOWDER, 300, 32, -1, -1, 2, null,
+			new Technology[] { Technology.REPLACEABLE_PARTS },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.BONUS_VS_TANK_10}),
+
+	ARTILLERY(CombatType.SIEGE, 420, 16, 32, 3, 2, null,
+			new Technology[] { Technology.DYNAMITE },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.RANGE_ATTACK, UnitActionType.PRE_ATTACK_SETUP },
+			new UnitAbility[] {UnitAbility.INDIRECT_FIRE, UnitAbility.BONUS_VS_CITY_10, UnitAbility.NO_DEFENSIVE_BONUS}),
+
+	INFANTRY(CombatType.GUNPOWDER, 300, 36, -1, -1, 2, null,
+			new Technology[] { Technology.REPLACEABLE_PARTS },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {}),
+
+	PANZER(CombatType.ARMORED, 450, 60, -1, -1, 5, null,
+			new Technology[] { Technology.COMBUSTION },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.LIMITED_VISIBILITY, UnitAbility.CAN_MOVE_AFTER_ATTACK}),
+
+	TANK(CombatType.ARMORED, 450, 50, -1, -1, 4, null,
+			new Technology[] { Technology.COMBUSTION },
+			new UnitActionType[] { UnitActionType.DELETE, UnitActionType.MOVE, UnitActionType.SLEEP_WAKE,
+					UnitActionType.ALERT, UnitActionType.FORTIFY, UnitActionType.PILLAGE,
+					UnitActionType.MELEE_ATTACK },
+			new UnitAbility[] {UnitAbility.PENALTY_VS_CITY_10, UnitAbility.NO_DEFENSIVE_BONUS, UnitAbility.CAN_MOVE_AFTER_ATTACK});
 
 	public final CombatType combatType;
 	public final int cost;
@@ -88,24 +186,34 @@ public enum UnitType{
 	public final int range;
 	public final int MP;
 	public final Resource resourceRequired;
-	public final boolean mayMeleeAttack;
 	public final Technology technologyRequired[];
+	public final UnitActionType actionTypes[];
+	public final UnitAbility unitAbilities[];
 
-
-
-	private UnitType(CombatType combatType, int cost, int combatStrength, int rangedCombatStrength, int range, int MP, Resource resourceRequired, boolean mayMeleeAttack, Technology technologyRequired[]){
-		this.combatType=combatType;
-		this.cost=cost;
-		this.combatStrength=combatStrength;
-		this.rangedCombatStrength=rangedCombatStrength;
-		this.range=range;
-		this.MP=MP;
-		this.resourceRequired=resourceRequired;
-		this.mayMeleeAttack=mayMeleeAttack;
-		this.technologyRequired=technologyRequired;
+	private UnitType(CombatType combatType, int cost, int combatStrength, int rangedCombatStrength, int range, int MP,
+			Resource resourceRequired, Technology technologyRequired[], UnitActionType actionTypes[],
+			UnitAbility unitAbilities[]) {
+		this.combatType = combatType;
+		this.cost = cost;
+		this.combatStrength = combatStrength;
+		this.rangedCombatStrength = rangedCombatStrength;
+		this.range = range;
+		this.MP = MP;
+		this.resourceRequired = resourceRequired;
+		this.technologyRequired = technologyRequired;
+		this.actionTypes = actionTypes;
+		this.unitAbilities = unitAbilities;
 	}
 
-	public Unit createUnit(Civilization owner, Tile tile){
+	public Unit createUnit(Civilization owner, Tile tile) {
 		return combatType.createUnit(this, owner, tile);
+	}
+
+	public boolean isActionAppliable(UnitActionType actionType) {
+		for (UnitActionType unitActionType : actionTypes) {
+			if (unitActionType == actionType)
+				return true;
+		}
+		return false;
 	}
 }
