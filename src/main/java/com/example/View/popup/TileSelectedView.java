9f8b71c8bcc1452145ab890b240c118acdb02eab
aryan <arywnansari@gmail.com>
2022-07-22 13:12:21 +0430

    update by aryan add smt

---
+++ src/main/java/com/example/View/popup/TileSelectedView.java
@@ -0,0 +1,11 @@
+package com.example.View.popup;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TileSelectedView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/example/View/popup/UnitSelectedView.java
@@ -0,0 +1,11 @@
+package com.example.View.popup;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UnitSelectedView extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
